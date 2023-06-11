843f688351fba33d55009461f9bea434ef43b8d6
melaatiptr <mltptri20@gmail.com>
2023-06-11 23:26:19 +0700

    Deploy

---
+++ app/Config/Routes.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const App = require("../Config/App.js");
+/**
+ * Express Routes class for defining API routes.
+ * @class
+ * @extends App
+ */
+class Routes extends App {
+  /**
+   * Constructs a new Routes object.
+   * Creates a new instance of the UsersController.
+   */
+  constructor() {
+    super();
+    this.user = new this.UsersController();
+    this.regist = new this.AuthRegister();
+    this.login = new this.AuthLogin();
+    this.upload = new this.UploadController();
+  }
+  /**
+   * Defines the routes for the API.
+   * @returns {Routes} An instance of the Express router containing the defined routes.
+   */
+  defineRoutes() {
+    const routes = express.Router();
+
+    routes.get("/test", this.user.testUser.bind(this.user));
+    routes.post("/testPost", this.user.createUser.bind(this.user));
+    routes.post("/Data", this.user.saveData.bind(this.user));
+    routes.post("/Register", this.regist.saveRegist.bind(this.regist));
+    routes.post("/Login", this.login.cekLogin.bind(this.login));
+    routes.post("/uploadTest", this.upload.uploadKonten.bind(this.upload));
+    routes.get("/getKonten", this.upload.getKonten.bind(this.upload));
+    routes.post("/cariKonten", this.upload.cariKonten.bind(this.upload));
+    return routes;
+  }
+}
+
+module.exports = Routes;
---
+++ app/Controllers/UploadController.js
@@ -0,0 +1,49 @@
+const BaseController = require("./BaseController.js");
+const path = require('path');
+class UploadController extends BaseController{
+
+    uploadKonten = async (req, res) => {
+        const {namaFoto, deskFoto, foto} = req.body;
+        if(req.file == null){
+            var pesan = {
+                messages: "Mohon upload foto produk"
+            };
+            return res.status(200).json(pesan)
+        }
+        const filePath = req.file.path;
+        var isGambar = path.basename(filePath);
+        let data = {
+            namaFoto: namaFoto,
+            deskFoto: deskFoto,
+            foto: isGambar
+        }
+        this.foto.save(data);
+        let response = {
+            status: 200,
+            messages: "Success Upload",
+            data: data
+        };
+        return res.status(200).json(response);
+    }
+
+    getKonten = async (req, res) => {
+        let konten = await this.foto.getResult();
+        let respon = {
+          "data":konten
+        }
+        res.status(200).json(respon);
+    }
+
+    cariKonten = async (req, res) => {
+        const {name} = req.body;
+        let konten = await this.foto.whereRaw(`namaFoto like "%${name}%"`).getResult();
+        let respon = {
+            "data": konten
+        }
+        res.status(200).json(respon);
+    }
+
+}
+
+
+module.exports = UploadController;
